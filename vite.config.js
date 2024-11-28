eb5fd4e707418e5d24240f38e0a47b173eed5d43
Blake <truongblake@gmail.com>
2024-11-27 17:15:31 -0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/portfolio",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        404: 'public/404.html',
+      },
+    },
+  },
 })
